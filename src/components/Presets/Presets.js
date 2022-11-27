919359a4c9812c316674dcba9f7105650ce0399a
Armin Sabouri <armin@team.casa>
2022-11-27 08:50:51 -0500

    Clean up presets

--- src/components/Presets/Presets.js
+++ src/components/Presets/Presets.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import './Presets.css';
+import "./Presets.css";
 
-import Select from '../controls/Select';
-import Item from '../Item';
+import Select from "../controls/Select";
+import Item from "../Item";
 
-import mining from './mining.json';
-import ports from './ports.json';
-import bandwidth from './bandwidth.json';
-import pruned from './pruned.json';
-import pi from './pi.json';
-import tor from './tor.json';
-import testnet from './testnet.json';
-import regtest from './regtest.json';
-import nosync from './nosync.json';
+import mining from "./mining.json";
+import ports from "./ports.json";
+import bandwidth from "./bandwidth.json";
+import pruned from "./pruned.json";
+import pi from "./pi.json";
+import tor from "./tor.json";
+import testnet from "./testnet.json";
+import regtest from "./regtest.json";
+import nosync from "./nosync.json";
 
-function toVal (val) {
+function toVal(val) {
   return { name: val, value: val };
 }
 
 // Determine which value to use, the default (a) or non-default (b)
-function mix (a, b) {
-  if (typeof a !== 'object' || typeof b !== 'object') {
+function mix(a, b) {
+  if (typeof a !== "object" || typeof b !== "object") {
     if (b !== undefined) {
       return b;
     }
     return b || a;
   }
 
-  Object.keys(a).forEach(key => {
+  Object.keys(a).forEach((key) => {
     a[key] = mix(a[key], b[key]);
   });
 
@@ -37,31 +37,22 @@ function mix (a, b) {
 }
 
 const presets = {
-  'Defaults': {},
-  'Mining': mining,
-  'Non-standard Ports': ports,
-  'Low Bandwidth': bandwidth,
-  'Low Disk Usage': pruned,
-  'Raspberry Pi': pi,
-  'Privacy': tor,
-  'Testnet': testnet,
-  'Regtest': regtest,
-  'Non-syncing': nosync,
+  Defaults: {},
 };
 
 class Presets extends Component {
   static propTypes = {
     preset: PropTypes.string,
     defaults: PropTypes.object.isRequired,
-    onChange: PropTypes.func.isRequired
+    onChange: PropTypes.func.isRequired,
   };
 
   static defaultProps = {
-    preset: 'Defaults'
+    preset: "Defaults",
   };
 
   change = (preset) => {
-    if (!window.confirm('Do you want to overwrite current config?')) {
+    if (!window.confirm("Do you want to overwrite current config?")) {
       this.forceUpdate();
       return;
     }
@@ -70,17 +61,17 @@ class Presets extends Component {
     this.props.onChange(preset, data);
   };
 
-  render () {
-    const {preset} = this.props;
+  render() {
+    const { preset } = this.props;
 
     return (
-      <div className='presets'>
-        <Item title={''} description={'Load predefined config.'}>
+      <div className="presets">
+        <Item title={""} description={"Load predefined config."}>
           <Select
             onChange={this.change}
             value={preset}
             values={Object.keys(presets).map(toVal)}
-            id={'presets'}
+            id={"presets"}
             disabled={false}
           />
         </Item>
@@ -89,7 +80,7 @@ class Presets extends Component {
   }
 }
 
-function clone (val) {
+function clone(val) {
   return JSON.parse(JSON.stringify(val));
 }
 
